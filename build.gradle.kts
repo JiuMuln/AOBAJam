72e749d044e2608fbc05d524f0a907287a35cd18
JiuMuln <69075711+JiuMuln@users.noreply.github.com>
2021-03-02 11:20:54 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     id("net.mamoe.mirai-console") version "2.4.1"
 }
 
-group = "org.example"
-version = "0.1.0"
+group = "com.github.jiumuln"
+version = "1.0"
 
 repositories {
     mavenLocal()
